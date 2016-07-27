57f21a2a453cbfdc3caa6dfe57986323b2a321a7
davidwelby <dwelby101@msn.com>
2016-07-27 21:47:51 +0100

    First commit

---
+++ app/heroes.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+    selector: 'my-heroes',
+    templateUrl: 'app/heroes.component.html',
+    styleUrls: ['app/heroes.component.css'],
+    directives: [],
+    providers: []
+})
+
+
+export class HeroesComponent implements OnInit {
+    title = 'Tour of Heroes';
+    heroes: Hero[];
+    selectedHero: Hero;
+
+    constructor(
+        private router: Router,
+        private heroService: HeroService) {
+
+    }
+
+    getHeroes() {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+
+    onSelect(hero: Hero) { this.selectedHero = hero; }
+
+    gotoDetail() {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+}
+
+
