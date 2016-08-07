95124014388cb40b1352418a5ae6cbd79fd0239b
David <dwelby101@msn.com>
2016-08-07 14:28:53 +0100

    Make menu sticky on scroll (#32)
    * Making sticky directive using custom window class...thingy
    
    * Set sticky point
    
    * Clean up file structure

---
+++ src/app/window.ts
@@ -0,0 +1,32 @@
+//****************************************
+// https://github.com/mikkeldamm/angular2-playground
+// https://medium.com/@MikkelDamm/sticky-header-in-angular2-90364eba81d9#.logqdvwwi
+//****************************************
+import {unimplemented} from '@angular/core/src/facade/exceptions';
+import {OpaqueToken, Provider} from '@angular/core/index';
+
+function _window(): any {
+    return window;
+}
+
+export const WINDOW: OpaqueToken = new OpaqueToken('WindowToken');
+
+export abstract class WindowRef {
+    get nativeWindow(): any {
+        return unimplemented();
+    }
+}
+
+export class BrowserWindowRef extends WindowRef {
+    constructor() {
+        super();
+    }
+    get nativeWindow(): any {
+        return _window();
+    }
+}
+
+export const WINDOW_PROVIDERS = [
+    new Provider(WindowRef, { useClass: BrowserWindowRef }),
+    new Provider(WINDOW, { useFactory: _window, deps: [] }),
+];
---
+++ src/directives/stick.directive.ts
@@ -0,0 +1,40 @@
+//****************************************
+// https://github.com/mikkeldamm/angular2-playground
+// https://medium.com/@MikkelDamm/sticky-header-in-angular2-90364eba81d9#.logqdvwwi
+//****************************************
+import {Directive, ElementRef, Input, HostListener} from '@angular/core';
+import {WindowRef} from '../window';
+
+@Directive({
+    selector: '[stick]'
+})
+export class StickDirective {
+
+    private _minY: number = 158;
+    private _className: string = 'navigation--stick';
+
+    @Input('stickMin') set stickMin(minY: number) {
+        this._minY = minY || this._minY;
+    }
+
+    @Input('stickClass') set stickClass(className: string) {
+        this._className = className || this._className;
+    }
+
+    constructor(private _element: ElementRef, private _window: WindowRef) {
+
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    handleScrollEvent(e) {
+
+        if (this._window.nativeWindow.pageYOffset > this._minY) {
+console.log('its hit the if.........');
+            this._element.nativeElement.classList.add(this._className);
+
+        } else {
+console.log('its hit the else.........');
+            this._element.nativeElement.classList.remove(this._className);
+        }
+    }
+}
